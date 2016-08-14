64cad0bf37aba9433dfbd7d4ef68a882ce5eea89
David Zhang <crispgm@gmail.com>
2016-08-14 13:23:55 +0800

    Test coverage 100% for test/command

--- test/test_command.rb
+++ test/test_command.rb
@@ -31,12 +31,6 @@ OUT
     assert_equal("info", @cmd.parse_cmd)
   end
 
-  def test_parse_info_with_less_param
-    argv = ['info']
-    @cmd = Heel::Command.new(argv)
-    assert_equal("usage", @cmd.parse_cmd)
-  end
-
   def test_parse_run
     argv = ['run', 'hello_world', 'hello, world']
     @cmd = Heel::Command.new(argv)
@@ -68,5 +62,23 @@ DESC
     assert_output(description) {
       assert_equal("usage", @cmd.parse_cmd)
     }
+  end
+
+  def test_parse_info_with_less_param
+    argv = ['info']
+    @cmd = Heel::Command.new(argv)
+    assert_equal("usage", @cmd.parse_cmd)
+  end
+
+  def test_parse_run_with_less_param
+    argv = ['run']
+    @cmd = Heel::Command.new(argv)
+    assert_equal("usage", @cmd.parse_cmd)
+  end
+
+  def test_parse_help_with_less_param
+    argv = ['help']
+    @cmd = Heel::Command.new(argv)
+    assert_equal("usage", @cmd.parse_cmd)
   end
 end
