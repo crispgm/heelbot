25d7a4d5da9e855f40541280c12c1ca3b8211044
David Zhang <crispgm@gmail.com>
2016-08-20 01:23:37 +0800

    Add msg subcommand

--- lib/heel/command.rb
+++ lib/heel/command.rb
@@ -18,6 +18,7 @@ Version
 
 Bot
     heel list
+    heel msg  [trigger_msg]
     heel help [bot_name]
     heel run  [bot_name]
     heel info [bot_name]
@@ -31,6 +32,9 @@ DESC
       if argv[0].eql? "list"
         list
         "list"
+      elsif argv[0].eql? "msg"
+        trigger_msg(argv.slice(1, argv.length))
+        "msg"
       elsif argv[0].eql? "help"
         if (argv.length <= 1)
           usage
@@ -63,6 +67,29 @@ DESC
     def list
       @bot_manager.bot_list.each do |bot|
         puts "#{bot["Name"]}"
+      end
+    end
+
+    def trigger_msg(argv)
+      @triggers = Hash.new
+      @bot_manager.bot_list.each do |bot|
+        @bot_manager.init_bot(bot["Name"])
+        bot_triggers = @bot_manager.get_triggers_of_bot
+        if !bot_triggers.empty?
+          bot_triggers.each do |trigger|
+            if @triggers.has_key? trigger
+              puts "Conflict: Trigger #{trigger} is existed."
+            end
+            @triggers[trigger] = bot["Name"]
+          end
+        end
+      end
+
+      @triggers.each do |trigger_text, bot_name|
+        if argv[0].start_with? trigger_text
+          # triggered, run bot
+          @bot_manager.run_bot(bot_name, argv.slice(1, argv.length))
+        end
       end
     end
 
