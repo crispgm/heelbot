4f9cb2940d173ee89250dc06f0b33fac626dc0f2
David Zhang <crispgm@gmail.com>
2016-08-24 15:57:08 +0800

    Update oct_movie

--- heelspec/oct_movie.rb
+++ heelspec/oct_movie.rb
@@ -23,20 +23,23 @@ module Heelspec
     end
 
     def serve(request)
-      { 
+      imgs = get_schedule_img
+      img_data = []
+      imgs.each do |img|
+        img_data << {
+          :title => "",
+          :text => "",
+          :color => "#666666",
+          :images => [
+            {
+              :url => img
+            }
+          ]
+        }
+      end
+      data = { 
         :text => "OCT Movie Schedule",
-        :attachments => [
-          {
-            :title => "",
-            :text => "",
-            :color => "#666666",
-            :images => [
-              {
-                :url => get_schedule_img
-              }
-            ]
-          }
-        ]
+        :attachments => img_data
       }
     end
 
@@ -44,9 +47,22 @@ module Heelspec
     def get_schedule_img
       page = Nokogiri::HTML(open(OCT_MOVIE_SCHEDULE_PAGE_URL))
       page.encoding = "UTF-8"
+      # get two pages url
+      url = []
+      page.css("ul.dropdown-menu-right")[0].css("li").each do |item|
+        url << ("" << OCT_MOVIE_DOMAIN_NAME << item.css("a")[0]["href"])
+      end
+      
+      # get photos
+      imgs = []
+      url.each do |page_url|
+        page = Nokogiri::HTML(open(page_url))
+        page.encoding = "UTF-8"
+        div = page.css("div.table-responsive").first
+        imgs << ("" << OCT_MOVIE_DOMAIN_NAME << div.css("img")[0]["src"])
+      end
 
-      div = page.css("div.table-responsive").first
-      "" << OCT_MOVIE_DOMAIN_NAME << div.css("img")[0]["src"]
+      imgs
     end
   end
 end
