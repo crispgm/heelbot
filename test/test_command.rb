2c2f218c3b70ee8ac70405d82a012e6a583b1a53
David Zhang <crispgm@gmail.com>
2016-08-26 15:08:47 +0800

    Convert to shoulda style

--- test/test_command.rb
+++ test/test_command.rb
@@ -2,78 +2,21 @@ require "helper"
 
 class TestCommand < Minitest::Test
   
-  def setup
-    @spec_path = "test/heelspec"
-  end
+  context "test command" do
+    setup do
+      @spec_path = "test/heelspec"
+    end
 
-  def test_init
-    argv = ['param1', 'param2', 'param3']
-    @cmd = Heel::Command.new(argv, @spec_path)
-    assert_equal(@cmd.argv.size, argv.size)
-  end
+    should "init with argv" do
+      argv = ['param1', 'param2', 'param3']
+      @cmd = Heel::Command.new(argv, @spec_path)
+      assert_equal(@cmd.argv.size, argv.size)
+    end
 
-  def test_parse_list
-    argv = ['list']
-    @cmd = Heel::Command.new(argv, @spec_path)
-    output = <<OUT
-hello_world
-test_bot
-OUT
-    assert_output(output) {
-      assert_equal("list", @cmd.parse_cmd)
-    }
-  end
-
-  def test_parse_msg_console
-    $runtime_mode = Heel::Util::RUNTIME_CONSOLE
-    argv = ['msg', '!hw', 'helloworld']
-    @cmd = Heel::Command.new(argv, @spec_path)
-    assert_equal("msg, [\"test_bot\", \"\"]", @cmd.parse_cmd)
-  end
-
-  def test_parse_msg2_console
-    $runtime_mode = Heel::Util::RUNTIME_CONSOLE
-    argv = ['msg', '!helloworld', 'helloworld']
-    @cmd = Heel::Command.new(argv, @spec_path)
-    assert_equal("msg, [\"hello_world\", \"\"]", @cmd.parse_cmd)
-  end
-
-  def test_parse_msg_web
-    $runtime_mode = Heel::Util::RUNTIME_WEB
-    argv = ['msg', '!hw', 'helloworld']
-    @cmd = Heel::Command.new(argv, @spec_path)
-    assert_equal("msg, [\"test_bot\", {:text=>\"Bot not implemented\"}]", @cmd.parse_cmd)
-  end
-
-  def test_parse_msg2_web
-    $runtime_mode = Heel::Util::RUNTIME_WEB
-    argv = ['msg', '!helloworld', 'helloworld']
-    @cmd = Heel::Command.new(argv, @spec_path)
-    assert_equal("msg, [\"hello_world\", {:text=>\"hello,world\"}]", @cmd.parse_cmd)
-  end
-
-  def test_parse_info
-    argv = ['info', 'hello_world']
-    @cmd = Heel::Command.new(argv, @spec_path)
-    assert_equal("info", @cmd.parse_cmd)
-  end
-
-  def test_parse_run
-    argv = ['run', 'hello_world', 'hello, world']
-    @cmd = Heel::Command.new(argv, @spec_path)
-    assert_equal("run", @cmd.parse_cmd)
-  end
-
-  def test_parse_help
-    argv = ['help', 'hello_world']
-    @cmd = Heel::Command.new(argv, @spec_path)
-    assert_equal("help", @cmd.parse_cmd)
-  end
-
-  def test_parse_usage
-    argv = ['param1', 'param2', 'param3']
-    @cmd = Heel::Command.new(argv, @spec_path)
-    description = <<DESC
+    should "show usage" do
+      argv = ['param1', 'param2', 'param3']
+      @cmd = Heel::Command.new(argv, @spec_path)
+      description = <<DESC
 Heelbot Usage:
 
 Version
@@ -86,27 +29,103 @@ Bot
     heel run  [bot_name]
     heel info [bot_name]
 
+New Bot
+    heel new  [bot_name]
+
 DESC
-    assert_output(description) {
-      assert_equal("usage", @cmd.parse_cmd)
-    }
-  end
+      assert_output(description) {
+        assert_equal("usage", @cmd.parse_cmd)
+      }
+    end
 
-  def test_parse_info_with_less_param
-    argv = ['info']
-    @cmd = Heel::Command.new(argv, @spec_path)
-    assert_equal("usage", @cmd.parse_cmd)
-  end
+    context "list command" do
+      should "list test bot" do
+        argv = ['list']
+        @cmd = Heel::Command.new(argv, @spec_path)
+        output = <<OUT
+hello_world
+test_bot
+OUT
+        assert_output(output) {
+          assert_equal("list", @cmd.parse_cmd)
+        }
+      end
+    end
 
-  def test_parse_run_with_less_param
-    argv = ['run']
-    @cmd = Heel::Command.new(argv, @spec_path)
-    assert_equal("usage", @cmd.parse_cmd)
-  end
+    context "run command" do
+      should "run bot" do
+        argv = ['run', 'hello_world', 'hello, world']
+        @cmd = Heel::Command.new(argv, @spec_path)
+        assert_equal("run", @cmd.parse_cmd)
+      end
 
-  def test_parse_help_with_less_param
-    argv = ['help']
-    @cmd = Heel::Command.new(argv, @spec_path)
-    assert_equal("usage", @cmd.parse_cmd)
+      should "invoke usage wihout enough params" do
+        argv = ['run']
+        @cmd = Heel::Command.new(argv, @spec_path)
+        assert_equal("usage", @cmd.parse_cmd)
+      end
+    end
+
+    context "info command" do
+      should "info bot" do
+        argv = ['info', 'hello_world']
+        @cmd = Heel::Command.new(argv, @spec_path)
+        assert_equal("info", @cmd.parse_cmd)
+      end
+
+      should "invoke usage wihout enough params" do
+        argv = ['info']
+        @cmd = Heel::Command.new(argv, @spec_path)
+        assert_equal("usage", @cmd.parse_cmd)
+      end
+    end
+
+    context "help command" do
+      should "help bot" do
+        argv = ['help', 'hello_world']
+        @cmd = Heel::Command.new(argv, @spec_path)
+        assert_equal("help", @cmd.parse_cmd)
+      end
+
+      should "invoke usage wihout enough params" do
+        argv = ['help']
+        @cmd = Heel::Command.new(argv, @spec_path)
+        assert_equal("usage", @cmd.parse_cmd)
+      end
+    end
+
+    context "msg command" do
+      context "runtime mode" do
+        should "invoke with trigger" do
+          $runtime_mode = Heel::Util::RUNTIME_CONSOLE
+          argv = ['msg', '!hw', 'helloworld']
+          @cmd = Heel::Command.new(argv, @spec_path)
+          assert_equal("msg, [\"test_bot\", \"\"]", @cmd.parse_cmd)
+        end
+
+        should "trigger other keyword" do
+          $runtime_mode = Heel::Util::RUNTIME_CONSOLE
+          argv = ['msg', '!helloworld', 'helloworld']
+          @cmd = Heel::Command.new(argv, @spec_path)
+          assert_equal("msg, [\"hello_world\", \"\"]", @cmd.parse_cmd)
+        end
+      end
+      
+      context "web mode" do
+        should "return bot not implemented when trigger bot without web mode" do
+          $runtime_mode = Heel::Util::RUNTIME_WEB
+          argv = ['msg', '!hw', 'helloworld']
+          @cmd = Heel::Command.new(argv, @spec_path)
+          assert_equal("msg, [\"test_bot\", {:text=>\"Bot not implemented\"}]", @cmd.parse_cmd)
+        end
+
+        should "invoke with trigger" do
+          $runtime_mode = Heel::Util::RUNTIME_WEB
+          argv = ['msg', '!helloworld', 'helloworld']
+          @cmd = Heel::Command.new(argv, @spec_path)
+          assert_equal("msg, [\"hello_world\", {:text=>\"hello,world\"}]", @cmd.parse_cmd)
+        end
+      end
+    end
   end
 end
