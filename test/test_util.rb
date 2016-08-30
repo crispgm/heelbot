c886dda163c105c36a16d3696ba3007c9167ef4e
David Zhang <crispgm@gmail.com>
2016-08-30 16:40:01 +0800

    Refactor to shoulda-context

--- test/test_util.rb
+++ test/test_util.rb
@@ -1,26 +1,32 @@
 require "helper"
 
 class TestUtil < Minitest::Test
-  def test_capture
-    output = Heel::Util.capture_stdout do
-      puts "hello, world"
+  context "test util functions" do
+    context "capture outputs" do
+      should "capture hello world as output" do
+        output = Heel::Util.capture_stdout do
+          puts "hello, world"
+        end
+        assert_equal("hello, world\n", output)
+      end
     end
-    assert_equal("hello, world\n", output)
-  end
 
-  def test_web_mode
-    $runtime_mode = Heel::Util::RUNTIME_WEB
-    assert_equal(true, Heel::Util.web_mode?)
-  end
+    context "get modes" do
+      should "return web mode" do
+        $runtime_mode = Heel::Util::RUNTIME_WEB
+        assert_equal(true, Heel::Util.web_mode?)
+      end
 
-  def test_console_mode
-    $runtime_mode = Heel::Util::RUNTIME_CONSOLE
-    assert_equal(true, Heel::Util.console_mode?)
-  end
+      should "return console mode" do
+        $runtime_mode = Heel::Util::RUNTIME_CONSOLE
+        assert_equal(true, Heel::Util.console_mode?)
+      end
 
-  def test_nil_global_runtime
-    $runtime_mode = nil
-    assert_equal(false, Heel::Util.console_mode?)
-    assert_equal(false, Heel::Util.web_mode?)
+      should "return false if global var is not set" do
+        $runtime_mode = nil
+        assert_equal(false, Heel::Util.console_mode?)
+        assert_equal(false, Heel::Util.web_mode?)
+      end
+    end
   end
 end
